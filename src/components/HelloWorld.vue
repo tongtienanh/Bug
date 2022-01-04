641c97aecc744ff3f258f0c4a4e175b133d1d658
tong_anh <anhtt@vuihoc.vn>
2022-01-04 15:03:08 +0700

    calendar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,51 +1,31 @@
 <template>
-  <v-card>
-    <v-tabs
-      v-model="tab"
-      background-color="primary"
-      dark
-    >
-      <v-tab
-        v-for="item in items"
-        :key="item.tab"
-      >
-        {{ item.tab }}
-      </v-tab>
-    </v-tabs>
-
-    <v-tabs-items v-model="tab">
-      <v-tab-item
-        v-for="(item, index) in items"
-        :key="item.tab"
-      >
-        <Item :key="item.tab" :current="tab" :tab="index" />
-      </v-tab-item>
-    </v-tabs-items>
+  <v-card contextmenu="center">
+    <DatePicker :value="null" color="red" is-range mode="dateTime" columns="2" />
   </v-card>
 </template>
 
 <script>
-  import Vue from 'vue'
-  import DatetimePicker from 'vuetify-datetime-picker';
-  Vue.use(DatetimePicker)
-  import Item from "./Item";
-  export default {
-    name: 'HelloWorld',
-    components: {Item},
-    data: () => ({
-      tab: 0,
-      items: [
-        { tab: 'One', content: 'Tab 1 Content' },
-        { tab: 'Two', content: 'Tab 2 Content' },
-        { tab: 'Three', content: 'Tab 3 Content' },
-        { tab: 'Four', content: 'Tab 4 Content' },
-        { tab: 'Five', content: 'Tab 5 Content' },
-        { tab: 'Six', content: 'Tab 6 Content' },
-        { tab: 'Seven', content: 'Tab 7 Content' },
-        { tab: 'Eight', content: 'Tab 8 Content' },
-        { tab: 'Nine', content: 'Tab 9 Content' },
-        { tab: 'Ten', content: 'Tab 10 Content' },
-      ],
-    }),
-  }
+import Vue from "vue";
+import DatetimePicker from "vuetify-datetime-picker";
+Vue.use(DatetimePicker);
+import DatePicker from "v-calendar/lib/components/date-picker.umd";
+export default {
+  name: "HelloWorld",
+  components: { DatePicker },
+  data: () => ({
+    tab: 0,
+    items: [
+      { tab: "One", content: "Tab 1 Content" },
+      { tab: "Two", content: "Tab 2 Content" },
+      { tab: "Three", content: "Tab 3 Content" },
+      { tab: "Four", content: "Tab 4 Content" },
+      { tab: "Five", content: "Tab 5 Content" },
+      { tab: "Six", content: "Tab 6 Content" },
+      { tab: "Seven", content: "Tab 7 Content" },
+      { tab: "Eight", content: "Tab 8 Content" },
+      { tab: "Nine", content: "Tab 9 Content" },
+      { tab: "Ten", content: "Tab 10 Content" },
+    ],
+  }),
+};
 </script>
--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,54 +1,53 @@
 <template>
-<div>
-    <div>Tab {{ tab }}</div>
-    <div v-if="tab == 1">
-        <div v-for="(item, index) in scheduleInfo" :key="index">{{ item.id }} - {{ item.name }}</div>
-    </div>
-</div>
+  <div>
+   
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Item",
-        props: ["tab", "current"],
-        data: function () {
-            return {
-                scheduleInfo: null
-            }
-        },
-        created() {
-            console.log(`${this.tab} is created.`);
-            if (this.tab == 1) {
-                this.fetchData();
-            }
-            
-        },
-        destroyed() {
-            console.log(`${this.tab} is destroyed.`)
-        },
-        mounted() {
-            console.log(`${this.tab} is mounted.`)
-        },
-        updated() {
-            console.log(`${this.tab} is updated.`);
-        },
-        watch: {
-            current: function (val, old) {
-                console.log(`New: ${val}, ${old}`);
-                if (this.tab == val) {
-                    this.fetchData();
-                }
-            }
-        },
-        methods: {
-            fetchData: async function() {
-                console.log("this.fetchData", this.tab);
-                this.scheduleInfo = [
-                    { id: 1, name: "Schedule 1" },
-                    { id: 2, name: "Schedule 2" },
-                    { id: 3, name: "Schedule 3" },
-                ];
-            }
-        }
+export default {
+  name: "Item",
+  props: ["tab", "current"],
+  components:{
+  },
+  data: function () {
+    return {
+      scheduleInfo: null,
+    };
+  },
+  created() {
+    console.log(this.$vuetify.application.top);
+    console.log(`${this.tab} is created.`);
+    if (this.tab == 1) {
+      this.fetchData();
     }
+  },
+  destroyed() {
+    console.log(`${this.tab} is destroyed.`);
+  },
+  mounted() {
+    console.log(`${this.tab} is mounted.`);
+  },
+  updated() {
+    console.log(`${this.tab} is updated.`);
+  },
+  watch: {
+    current: function (val, old) {
+      console.log(`New: ${val}, ${old}`);
+      if (this.tab == val) {
+        this.fetchData();
+      }
+    },
+  },
+  methods: {
+    fetchData: async function () {
+      console.log("this.fetchData", this.tab);
+      this.scheduleInfo = [
+        { id: 1, name: "Schedule 1" },
+        { id: 2, name: "Schedule 2" },
+        { id: 3, name: "Schedule 3" },
+      ];
+    },
+  },
+};
 </script>
