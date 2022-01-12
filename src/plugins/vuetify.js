37e6c59ab78542704c8569a2cb9bbee672c675a9
tong_anh <anhtt@vuihoc.vn>
2022-01-12 16:34:59 +0700

    fix

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,8 +1,21 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
-import 'vuetify/dist/vuetify.min.css'
+import 'vuetify/dist/vuetify.min.css';
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            primary: colors.green.accent3,
+            secondary: colors.deepOrange.lighten1
+          },
+          dark: {
+            primary: '#90CAF9',
+            secondary: '#E91E63'
+          },
+        },
+      },
 });
